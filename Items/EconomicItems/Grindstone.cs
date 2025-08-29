acea6e969cf5276062dff3393819d8d0e27acd40
MrSirnik <nektotim21@gmail.com>
2025-08-29 20:24:03 +0300

    added a loss of armor strength and the realization of a grindstone

---
+++ Items/EconomicItems/Grindstone.cs
@@ -0,0 +1,13 @@
+﻿namespace GamePrototype.Items.EconomicItems
+{
+    public sealed class Grindstone : EconomicItem
+    {
+        //public uint UseGrindstone => 4;
+        public override bool Stackable => false;
+
+        public Grindstone(string name) : base(name)
+        {
+
+        }    
+    }
+}
---
+++ Items/EquipItems/EquipItem.cs
@@ -0,0 +1,24 @@
+﻿using GamePrototype.Items.EconomicItems;
+using GamePrototype.Utils;
+
+namespace GamePrototype.Items.EquipItems
+{
+    public abstract class EquipItem : Item
+    {
+        private uint _durability;
+        private uint _maxDurability;
+        public uint Durability { get => _durability; protected set => _durability = value; }
+        public override bool Stackable => false;
+
+        public abstract EquipSlot Slot { get; }
+
+        protected EquipItem(uint maxDurability, string name) : base(name) => _maxDurability = maxDurability;
+
+        public void ReduceDurability(uint delta) => _durability -= delta;//прочность--
+
+        public void Repair(uint delta) => //ремонт++
+            _durability += _durability + delta > _maxDurability 
+            ? _maxDurability 
+            : _durability + delta;
+    }
+}
